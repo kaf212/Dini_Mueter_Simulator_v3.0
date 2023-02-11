dc328c55e9b1f6bf6ff946a9526309dca232d401
kaf212 <kaf221122@gmail.com>
2023-02-11 11:48:41 +0100

    added and implemented initialize_items() and find_item()

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from datetime import datetime
 
 @dataclass
 class Item:
+    id: str
     name: str
     top_level_category: str
     category: str
@@ -83,52 +84,119 @@ class Item:
 
 
 def initialize_items():
-    return
+    return [Item('colt_m1911', 'Colt M1911', 'firearm', 'handgun', 'E pistole halt', 20.0, 1, 0, -25, -25, 30, -40, 0),
+            Item('mk1_frag_grenade', 'Mk.1 Splittergranate', 'explosive', 'anti personnel',
+                 'Tätscht und verteilt Metall-Konfetti', 30.0, 1, 0, -50, -40, 40, -50, 0),
+            Item('rpg_7', 'RPG-7', 'explosive', 'anti tank', 'Nöd hine ineluege', 100.0, 3, -100, -50, -75, 50, -50, 0),
+            Item('m16a1', 'M16A1', 'firearm', 'assault rifle', 'Wahre Klassiker', price=45.0, req_skill_lv=2, infl_mass=0,
+                 infl_health=-15, infl_mood=-10, infl_anger=20, infl_boredom=-15, infl_confusion=0),
+            Item('m1_garand', 'M1 Garand', 'firearm', 'rifle', 'Tönt kuul bim Nahlade', price=20.0, req_skill_lv=2,
+                 infl_mass=0, infl_health=-10, infl_mood=-10, infl_anger=15, infl_boredom=-15, infl_confusion=0),
+            Item('m24', 'M24', 'firearm', 'rifle', 'De Siech isch scheisse lut aber fäggt', price=50.0, req_skill_lv=3,
+                 infl_mass=0, infl_health=-35, infl_mood=-15, infl_anger=20, infl_boredom=-15, infl_confusion=0),
+            Item('kar98k', 'Kar98k', 'firearm', 'rifle', 'Gar nöd eso churz', price=75.0, req_skill_lv=4,
+                 infl_mass=0, infl_health=-25, infl_mood=-15, infl_anger=20, infl_boredom=-15, infl_confusion=0),
+            Item('stg_44', 'StG 44', 'firearm', 'assault rifle', 'Ich han die imfall vergoldet in Battlefield V', price=80.0,
+                 req_skill_lv=4, infl_mass=0, infl_health=-20, infl_mood=-15, infl_anger=15, infl_boredom=-15,
+                 infl_confusion=0),
+            Item('glock_17', 'Glock 17', 'firearm', 'handgun', "D'Öschis wüssed wies gaht", price=40.0, req_skill_lv=5,
+                 infl_mass=0, infl_health=-10, infl_mood=-10, infl_anger=15, infl_boredom=-15, infl_confusion=0),
+            Item('medkit', 'Medikit', 'consumable', 'medical', 'Universale Hälfer', price=10.0, req_skill_lv=1, infl_mass=0,
+                 infl_health=30, infl_mood=20, infl_anger=-20, infl_boredom=0, infl_confusion=0),
+            Item('diamond_pickaxe', 'Diamante Pickaxe', 'videogame', 'minecraft',
+                 'Alte, mit dem chasch fucking Obsidian abbaue', price=100.0, req_skill_lv=10, infl_mass=-50,
+                 infl_health=-50, infl_mood=-20, infl_anger=30, infl_boredom=0, infl_confusion=15),
+            Item('secret_firedragon', 'De geheimi Fürdrache', 'meme', 'insider', 'Alte, er hätt de geheimi Fürdrache!',
+                 price=10000.0, req_skill_lv=100, infl_mass=0, infl_health=-200, infl_mood=-150,
+                 infl_anger=100,
+                 infl_boredom=-100, infl_confusion=100),
+            Item('anti_horny_bat', 'Anti-Hornig-Schleger', 'meme', 'classic', 'Gang is hornig-Gfängniss', price=20.0,
+                 req_skill_lv=4, infl_mass=0, infl_health=-5, infl_mood=-20, infl_anger=30,
+                 infl_boredom=0, infl_confusion=5)
+
+            ]
 
 
-item_colt_m1911 = Item('Colt M1911', 'firearm', 'handgun', 'E pistole halt', 20.0, 1, 0, -25, -25, 30, -40, 0)
+# item_colt_m1911 = Item('Colt M1911', 'firearm', 'handgun', 'E pistole halt', 20.0, 1, 0, -25, -25, 30, -40, 0)
+#
+# item_mk_1_handgrenade = Item('Mk.1 Splittergranate', 'explosive', 'anti personnel',
+#                              'Tätscht und verteilt Metall-Konfetti', 30.0, 1, 0, -50, -40, 40, -50, 0)
+#
+# item_rpg_7 = Item('RPG-7', 'explosive', 'anti tank', 'Nöd hine ineluege', 100.0, 3, -100, -50, -75, 50, -50, 0)
+#
+# item_m16a1 = Item('M16A1', 'firearm', 'assault rifle', 'Wahre Klassiker', price=45.0, req_skill_lv=2, infl_mass=0,
+#                   infl_health=-15, infl_mood=-10, infl_anger=20, infl_boredom=-15, infl_confusion=0)
+#
+# item_m1_garand = Item('M1 Garand', 'firearm', 'rifle', 'Tönt kuul bim Nahlade', price=20.0, req_skill_lv=2,
+#                       infl_mass=0, infl_health=-10, infl_mood=-10, infl_anger=15, infl_boredom=-15, infl_confusion=0)
+#
+# item_m24 = Item('M24', 'firearm', 'rifle', 'De Siech isch scheisse lut aber fäggt', price=50.0, req_skill_lv=3,
+#                 infl_mass=0, infl_health=-35, infl_mood=-15, infl_anger=20, infl_boredom=-15, infl_confusion=0)
+#
+# item_kar98k = Item('Kar98k', 'firearm', 'rifle', 'Gar nöd eso churz', price=75.0, req_skill_lv=4,
+#                    infl_mass=0, infl_health=-25, infl_mood=-15, infl_anger=20, infl_boredom=-15, infl_confusion=0)
+#
+# item_stg_44 = Item('StG 44', 'firearm', 'assault rifle', 'Ich han die imfall vergoldet in Battlefield V', price=80.0,
+#                    req_skill_lv=4, infl_mass=0, infl_health=-20, infl_mood=-15, infl_anger=15, infl_boredom=-15,
+#                    infl_confusion=0)
+#
+# item_glock_17 = Item('Glock 17', 'firearm', 'handgun', "D'Öschis wüssed wies gaht", price=40.0, req_skill_lv=5,
+#                      infl_mass=0, infl_health=-10, infl_mood=-10, infl_anger=15, infl_boredom=-15, infl_confusion=0)
+#
+# item_medkit = Item('Medikit', 'consumable', 'medical', 'Universale Hälfer', price=10.0, req_skill_lv=1, infl_mass=0,
+#                    infl_health=30, infl_mood=20, infl_anger=-20, infl_boredom=0, infl_confusion=0)
+#
+# item_diamond_pickaxe = Item('Diamante Pickaxe', 'videogame', 'minecraft',
+#                             'Alte, mit dem chasch fucking Obsidian abbaue', price=100.0, req_skill_lv=10, infl_mass=-50,
+#                             infl_health=-50, infl_mood=-20, infl_anger=30, infl_boredom=0, infl_confusion=15)
+# item_secret_firedragon = Item('De geheimi Fürdrache', 'meme', 'insider', 'Alte, er hätt de geheimi Fürdrache!',
+#                               price=10000.0, req_skill_lv=100, infl_mass=0, infl_health=-200, infl_mood=-150,
+#   #                             infl_anger=100,
+#                               infl_boredom=-100, infl_confusion=100)
+# item_horny_bat = Item('Anti-Hornig-Schleger', 'meme', 'classic', 'Gang is hornig-Gfängniss', price=20.0,
+#                       req_skill_lv=4, infl_mass=0, infl_health=-5, infl_mood=-20, infl_anger=30,
+#                       infl_boredom=0, infl_confusion=5)
 
-item_mk_1_handgrenade = Item('Mk.1 Splittergranate', 'explosive', 'anti personnel',
-                             'Tätscht und verteilt Metall-Konfetti', 30.0, 1, 0, -50, -40, 40, -50, 0)
+# all_items = [item_colt_m1911, item_mk_1_handgrenade, item_rpg_7, item_m16a1, item_m1_garand,
+#              item_m24, item_kar98k, item_stg_44, item_glock_17, item_medkit, item_diamond_pickaxe,
+#              item_secret_firedragon, item_horny_bat]
 
-item_rpg_7 = Item('RPG-7', 'explosive', 'anti tank', 'Nöd hine ineluege', 100.0, 3, -100, -50, -75, 50, -50, 0)
+all_items = initialize_items()
 
-item_m16a1 = Item('M16A1', 'firearm', 'assault rifle', 'Wahre Klassiker', price=45.0, req_skill_lv=2, infl_mass=0,
-                  infl_health=-15, infl_mood=-10, infl_anger=20, infl_boredom=-15, infl_confusion=0)
 
-item_m1_garand = Item('M1 Garand', 'firearm', 'rifle', 'Tönt kuul bim Nahlade', price=20.0, req_skill_lv=2,
-                      infl_mass=0, infl_health=-10, infl_mood=-10, infl_anger=15, infl_boredom=-15, infl_confusion=0)
+def find_item(search_criteria, search_term_list):
+    found_items = []
+    if search_criteria == 'id':
+        for item in all_items:
+            if item.id in search_term_list:
+                found_items.append(item)
 
-item_m24 = Item('M24', 'firearm', 'rifle', 'De Siech isch scheisse lut aber fäggt', price=50.0, req_skill_lv=3,
-                infl_mass=0, infl_health=-35, infl_mood=-15, infl_anger=20, infl_boredom=-15, infl_confusion=0)
+    elif search_criteria == 'top_level_category':
+        for item in all_items:
+            if item.top_level_category in search_term_list:
+                found_items.append(item)
 
-item_kar98k = Item('Kar98k', 'firearm', 'rifle', 'Gar nöd eso churz', price=75.0, req_skill_lv=4,
-                   infl_mass=0, infl_health=-25, infl_mood=-15, infl_anger=20, infl_boredom=-15, infl_confusion=0)
+    elif search_criteria == 'category':
+        for item in all_items:
+            if item.category in search_term_list:
+                found_items.append(item)
 
-item_stg_44 = Item('StG 44', 'firearm', 'assault rifle', 'Ich han die imfall vergoldet in Battlefield V', price=80.0,
-                   req_skill_lv=4, infl_mass=0, infl_health=-20, infl_mood=-15, infl_anger=15, infl_boredom=-15,
-                   infl_confusion=0)
+    elif search_criteria == 'req_skill_lv':
+        for item in all_items:
+            if item.req_skill_lv in search_term_list:
+                found_items.append(item)
 
-item_glock_17 = Item('Glock 17', 'firearm', 'handgun', "D'Öschis wüssed wies gaht", price=40.0, req_skill_lv=5,
-                     infl_mass=0, infl_health=-10, infl_mood=-10, infl_anger=15, infl_boredom=-15, infl_confusion=0)
+        else:
+            input('invalid search criteria, check find_item() for debugging. ')
 
-item_medkit = Item('Medikit', 'consumable', 'medical', 'Universale Hälfer', price=10.0, req_skill_lv=1, infl_mass=0,
-                   infl_health=30, infl_mood=20, infl_anger=-20, infl_boredom=0, infl_confusion=0)
+    found_items_quantity = 0
+    for found_item in found_items:
+        found_items_quantity += 1
 
-item_diamond_pickaxe = Item('Diamante Pickaxe', 'videogame', 'minecraft',
-                            'Alte, mit dem chasch fucking Obsidian abbaue', price=100.0, req_skill_lv=10, infl_mass=-50,
-                            infl_health=-50, infl_mood=-20, infl_anger=30, infl_boredom=0, infl_confusion=15)
-item_secret_firedragon = Item('De geheimi Fürdrache', 'meme', 'insider', 'Alte, er hätt de geheimi Fürdrache!',
-                              price=10000.0, req_skill_lv=100, infl_mass=0, infl_health=-200, infl_mood=-150,
-                              infl_anger=100,
-                              infl_boredom=-100, infl_confusion=100)
-item_horny_bat = Item('Anti-Hornig-Schleger', 'meme', 'classic', 'Gang is hornig-Gfängniss', price=20.0,
-                      req_skill_lv=4, infl_mass=0, infl_health=-5, infl_mood=-20, infl_anger=30,
-                      infl_boredom=0, infl_confusion=5)
-
-all_items = [item_colt_m1911, item_mk_1_handgrenade, item_rpg_7, item_m16a1, item_m1_garand,
-             item_m24, item_kar98k, item_stg_44, item_glock_17, item_medkit, item_diamond_pickaxe,
-             item_secret_firedragon, item_horny_bat]
+    if found_items_quantity > 1:   # you have no idea how long it took me to come up with this fix
+        return found_items
+    if found_items_quantity == 1:
+        return found_items[0]
 
 
 # --------- item stuff ----------------
@@ -143,7 +211,7 @@ class Player:
 
 
 player = Player(skill_lv=1, xp=90, balance=500.0)
-player_inventory = [item_colt_m1911, item_m16a1, item_rpg_7, item_medkit]
+player_inventory = find_item('id', ['colt_m1911', 'm16a1', 'rpg_7', 'medkit'])
 player_stocks = ['Microsoft', 'Microsoft', 'Tesla', 'Bitcoin']
 
 # --------- player stuff ------
@@ -277,16 +345,16 @@ def show_items(item_list, printed_properties):
     if printed_properties == 'show all properties':
         for item in item_list:
             print('--------------------------')
-            print(str(item))
+            print(item)
             print('--------------------------')
             input()
     elif printed_properties == 'show only names':
         for item in item_list:
-            print(f'Nr. {str(item_list.index(item))}: {str(item.name)}')
+            print(f'Nr. {item_list.index(item)}: {item.name}')
         input()
     elif printed_properties == 'show only names and price':
         for item in item_list:
-            print(f'Nr. {str(item_list.index(item))}: {str(item.name)}  CHF {str(item.price)}')
+            print(f'Nr. {item_list.index(item)}: {item.name}  CHF {item.price}')
         input()
     else:
         print('ERROR: printed_properties parameter not correctly defined (show_items())')
@@ -437,7 +505,8 @@ def add_achievement(achievement):
 # -------------------------------------------- achievements ------------------------------------------
 # -------------------------------------------- achievements ------------------------------------------
 def show_player_statistics():
-    user_selection = input_selection(['g', 'i', 'f'], ['Game Statistik', 'Item Statistik', 'Finanzielli Statistik'], 'Welli Statistik wetsch du ahluege?')
+    user_selection = input_selection(['g', 'i', 'f'], ['Game Statistik', 'Item Statistik', 'Finanzielli Statistik'],
+                                     'Welli Statistik wetsch du ahluege?')
     try:
         if user_selection == 'g':
             for key, value in player_data_game.items():
@@ -457,6 +526,8 @@ def show_player_statistics():
               'debugging. ')
     else:
         pass
+
+
 # -------------------------------------------- achievements ------------------------------------------
 
 # --------------------------------------------- shop --------------------------------------------
@@ -493,6 +564,7 @@ def transact_money(amount):
         player_data_financial['total_earnings'] += amount
     else:
         player_data_financial['total_spendings'] -= amount  # total amount should be shown positive in statistics
+
 
 # -------------- transaction ------------------
 
@@ -1079,7 +1151,8 @@ def enter_cheat_code():
         input('Cheat Code aktiviert - Du häsch 1 Kg Weed becho. ')
 
     elif user_cheat_code == 'TRUPP26':
-        player_inventory.append(item_secret_firedragon)
+        item = find_item('id', ['secret_firedragon'])
+        player_inventory.append(item)
         input('Cheat Code aktiviert - Du häsch de geheimi Fürdrache becho!!!!!!!')
 
     elif user_cheat_code == 'DEFYNNISCHENSPAST':
@@ -1152,9 +1225,13 @@ def end_program(optional_message):
         exit()
 
 
-transact_money(-1000)
-transact_money(69)
-show_player_statistics()
+# transact_money(-1000)
+# transact_money(69)
+# show_player_statistics()
 # add_achievement(achievement_4)
 # show_player_achievements()
+# item = find_item('id', ['secret_firedragon'])
+# print(item)
+main()
+
 # ------------------------------------ main ----------------------------------------------------
